7e992698125839f2e1756e4e5a1a7a1d4f0795e1
fnixdev <luisgatn000@gmail.com>
2022-10-19 11:37:58 -0300

    change cmd  stop -> delchat

--- useless/plugins/useless.py
+++ useless/plugins/useless.py
@@ -99,7 +99,7 @@ async def set_language(_, cb: CallbackQuery):
     await cb.edit_message_text(f"Information will be sent in {lang}")
 
 
-@useless.on_message(filters.command("stop", trg))
+@useless.on_message(filters.command("delchat", trg))
 async def stop_infos(_, message):
     chat_id = message.chat.id
     if not message.chat.type == ChatType.SUPERGROUP:
@@ -140,11 +140,18 @@ async def info():
 
 
 RANDOM = [
-    "No number from 1 to 999 includes the letter \"a\" in its word form.",
-    "The opposite sides of a die will always add up to seven.",
-    "You are 13.8 percent more likely to die on your birthday.",
-    "Playing dance music can help ward off mosquitoes.",
-    "If you open your eyes in a pitch-black room, the color you'll see is called \"eigengrau\".",
-    "Cats can't taste sweet things because of a genetic defect.",
-    "Pogonophobia is the fear of beards."
+    "Flamingos can only eat with their heads upside down.\n\nThese tall pink characters use their tongue as a sieve to catch food by flipping their heads about. The flamingo’s tongue helps pump the food-filled water in and out of their mouths about three times a second. This technique is called filter feeding! ",
+    "A blob of toothpaste is called a nurdle.\n\nNobody is quite sure where the word 'nurdle' originated. A fact as useless as the word.",
+    "The moon has moonquakes.\n\nShallow moonquakes are caused when the moon's crust slips and cracks due to the gradual shrinking of the moon when it cools. Meteors can also cause quakes when they crash into the surface of the moon.",
+    "People used to believe that kissing a donkey could relieve a toothache.\n\nYep, that’s right!  People have always done rather odd things to prevent toothache. For example, the Ancient Egyptians scrubbed their teeth with a powder made from ox hooves and eggshells. But weirder still, during the Middle Ages in Germany, they thought a cure for a toothache was kissing a donkey. Even if it did something for your toothache, it certainly won't do anything for your breath.",
+    "If you open your eyes in a pitch-black room, the colour you'll see is called 'eigengrau'.\n\n‘Eigengrau’ is German for ‘intrinsic grey’, also known as dark light, or brain grey. It is used to describe the uniform dark grey background that many people see in the absence of light. The term dates back to the nineteenth century.",
+    "Rabbits can’t vomit.\n\nWelcome to the vomit void. Bunny rabbits are very hygienic creatures who self-groom in much the same way that a cat does. But the rabbit’s digestive system does not move in reverse, meaning rabbits cannot bring up hairballs like a cat can. So basically if you're banking on a burst of bunny barf, don't hold your breath.",
+    "Alfred Hitchcock didn’t have a bellybutton.\n\nThe famous director of The Birds and Rear Window was born with one, but after surgery, it vanished after he was sewn back up! ",
+    "Bees sometimes sting other bees.\n\nThey don't mean to, but sometimes when they try to defend their nests from intruders, they accidentally sting other bees. Ouch!",
+    "There are 32 muscles in a cat’s ear.\n\nThis feature is what allows cats to swivel and rotate their ears so that they can pinpoint the source of a noise. But what's more incredible is that they can move each ear independently and rotate them 180 degrees!",
+    "There are more chickens in England than people.\n\nThere are around 982 million chickens eaten each year in the U.K. by 66 million people...  this statistic means that should the chickens decide to take over the country, (a la Jurassic Park) we would be massively outnumbered. The threat is real, people. Come the chicken uprising don't say we didn't warn you.",
+    "Crows hold grudges.\n\nA 2011 study revealed crows can remember the human faces of those who capture them. According to another study, ravens, including crows, jays and magpies, can 'hold grudges' for up to two years. So make sure you don't upset a crow, because they just won't let it lie!",
+    "‘Hippopotomonstrosesquippedaliophobia' is a fear of long words.",
+    "Due to a genetic defect, cats can’t taste sweet things.\n\nThis is because they are missing a taste receptor gene that allows their brains to recognise sweet tastes. No wonder they're often grumpy!",
+    "Tornadoes can make it ‘rain’ fish.\n\nThis phenomenon usually happens when swirling whirlwinds over shallow water develop into waterspouts that suck up water and the things living in it, like frogs, eels and fish. The creatures are carried long distances by clouds, and then drop to the ground like a mic.",
 ]
